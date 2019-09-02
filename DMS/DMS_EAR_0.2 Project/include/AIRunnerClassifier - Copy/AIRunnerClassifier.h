0cfb6875c9142790ff4e595dd87536bea2d0e061
karljiang118440 <jiang_chaoqing@126.com>
2019-09-02 16:13:09 +0800

    AIRunnerClassifier.h

--- DMS/DMS_EAR_0.2 Project/include/AIRunnerClassifier - Copy/AIRunnerClassifier.h
+++ DMS/DMS_EAR_0.2 Project/include/AIRunnerClassifier - Copy/AIRunnerClassifier.h
@@ -4,3 +4,29 @@
 #define AIRUNNERCLASSIFY_H_
 
 #include <string>
+
+
+
+class AIRunner_Classify
+{
+  public:
+   AIRunner_Classify(void);
+   ~AIRunner_Classify(void);
+
+  private:
+
+    Tensor* lApexNetInput;
+    std::vector<Tensor*>fixedOutput;
+    Tensor* floatInterm;
+
+    std::unique_ptr<Graph> net_mobile;
+    std::unique_ptr<Graph> netFixedToFloat;
+
+    std::vector<Tensor*> Output;
+    std::vector<std::string> classLabels;
+
+  public:
+
+  int case_mobilenet(const std::string& aMnetGraph,const std::string& aImageFile,const std::string& aSlimLabelsFile);
+  std::vector<std::pair<int,float>> case_mobilenet_target()
+}
