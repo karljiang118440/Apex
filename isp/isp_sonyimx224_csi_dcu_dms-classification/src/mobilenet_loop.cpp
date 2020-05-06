2862a69211479231122ec63cc4440539406ed6c7
karljiang118440 <32283913+karljiang118440@users.noreply.github.com>
2020-05-06 10:44:23 +0800

    add camera

--- isp/isp_sonyimx224_csi_dcu_dms-classification/src/mobilenet_loop.cpp
+++ isp/isp_sonyimx224_csi_dcu_dms-classification/src/mobilenet_loop.cpp
@@ -46,9 +46,8 @@
 #include "test_cases.hpp"
 #include <time.h>
 
-#define video_airunner false
-#define dcu false
-#define CAMERA false
+#define video_airunner 1
+#define dcu flase
 
 using Clock = std::chrono::high_resolution_clock;
 
@@ -63,23 +62,6 @@ using Clock = std::chrono::high_resolution_clock;
 #define CHNL_CNT io::IO_DATA_CH3
 
 #endif
-
-
-
-/* CAMERA
-
-定义外部输入摄像头信号，使用 sdi_lframe
-
-#1.貌似这样引用语法出错
-
-*/
-#ifdef CAMERA
-//extern ISPCameraInputStereo air_camera_input;
-extern SDI_Frame lFrame;
-
-#endif
-
-
 
 //****************karl2**************
 
@@ -167,11 +149,16 @@ int mobilenet_loop(const std::string& aMnetGraph, const std::string& aResultGrap
   }
   
   status = net_mobile->Prepare(); 
-  
+
+ std::cout << "mark1" << std::endl;
+
   if (Status_t::SUCCESS != status){
     std::cout << "APEX net verification failed" << std::endl;
     return -1;
   }
+
+
+std::cout << "mark2" << std::endl;
 
   int dimN = fixedOutput[0]->Dim(3);
   int dimH = fixedOutput[0]->Dim(2);
@@ -184,7 +171,10 @@ int mobilenet_loop(const std::string& aMnetGraph, const std::string& aResultGrap
                         TensorLayout<TensorFormat_t::NHWC>())));
   
   floatInterm->Allocate(Allocation_t::HEAP);
-  
+
+
+ std::cout << "mark2" << std::endl;
+
   // Final Output.
   std::vector<Tensor*> Output;
   status = LoadNetFromTensorFlow(*netFixedToFloat, aResultGraph, floatInterm, Output);
@@ -193,11 +183,14 @@ int mobilenet_loop(const std::string& aMnetGraph, const std::string& aResultGrap
     return -1;
   }
 
+std::cout << "mark1" << std::endl;
+
   status = netFixedToFloat->SetTargetHint(target::REF);
   if(Status_t::SUCCESS != status){
     std::cout << "Set APEX target failed" << std::endl;
     return -1;
   }
+std::cout << "mark3" << std::endl;
 
   status = netFixedToFloat->Prepare(); 
   if (Status_t::SUCCESS != status){
@@ -249,8 +242,11 @@ int mobilenet_loop(const std::string& aMnetGraph, const std::string& aResultGrap
    //cv::Mat outputImage = cv::Mat::zeros(cv::Size(1280, 720), CV_8UC3);
    cv::Mat outputImage;
 
+
   int Frame_interval = 30;
   int frame_count = 0;
+
+
 
 #endif
 
@@ -264,57 +260,6 @@ int mobilenet_loop(const std::string& aMnetGraph, const std::string& aResultGrap
                       io::IO_DATA_CH3);
 
 #endif                 
-
-
-
-#ifdef CAMERA
-
-	  // Get Frames
-    /*
-				 ai_camerainput.GetFrame(FDMA_IX_RGB888, lFrameRgb);
-				// ai_camerainput.GetFrame(FDMA_IX_G_SUB_2, lFrameRgb);
-				 if(lFrameRgb.mUMat.empty())
-				 {
-				   printf("Failed to pop the RGB frame.\n");
-				   continue;
-				 }
-
-			  outputImage = lFrameRgb.mUMat.getMat(vsdk::ACCESS_RW | OAL_USAGE_CACHED);
-			  if(!outputImage.data)
-			  {
-				std::cout << "ReadImageToTensor: Could not open or find image" << std::endl;
-				return {};
-			  }
-
-
-
-    */
-
-
-    lFrame = arContext.mpGrabber->FramePop();
-    if(lFrame.mUMat.empty())
-    {
-      printf("Failed to grab image number %u\n", arContext.mFrmCnt);
-      arContext.mError = true;
-      break;
-    } // if pop failed
-
-			  outputImage = lFrame.mUMat.getMat(vsdk::ACCESS_RW | OAL_USAGE_CACHED);
-			  if(!outputImage.data)
-			  {
-				std::cout << "ReadImageToTensor: Could not open or find image" << std::endl;
-				return {};
-			  }
-
-
-
-#endif
-
-
-
-
-
-
 
     
     while(true)
@@ -338,14 +283,6 @@ int mobilenet_loop(const std::string& aMnetGraph, const std::string& aResultGrap
 vsdk::Mat output_umat = vsdk::Mat(720, 1280, CV_8UC3); 
 
 //output_umat = outputImage.getUMat(cv::ACCESS_RW | OAL_USAGE_CACHED);
-
-
-/*CAMERA
-从 CAMERA 抓取摄像头
-
-*/
-
-
 
 
       stopwatch(true);
@@ -440,6 +377,204 @@ printf( "frame_count2 = %d \n", frame_count );
     }
 
 
+
+  return 0;
+}
+
+
+int mobilenet_loop_Camera(const std::string& aMnetGraph, 
+                   const std::string& aResultGraph, 
+                   const cv::Mat Camera_input, 
+                   const std::string& aSlimLabelsFile)
+{
+
+
+  Status_t status;
+
+  // Creating a new workspace  
+  TargetInfo lRefInfo = TargetInfo();
+  ApexTargetInfo lApexInfo = ApexTargetInfo();
+  auto lWorkspace = std::unique_ptr<Workspace>(new Workspace(std::map<std::string, TargetInfo*>{
+      {target::REF, &lRefInfo}, {target::APEX, &lApexInfo}}));
+
+  // Create and populate the Graphs
+  auto net_mobile = std::unique_ptr<Graph>(new Graph (lWorkspace.get())); 
+  auto netFixedToFloat = std::unique_ptr<Graph>(new Graph (lWorkspace.get()));
+
+  Tensor* lApexNetInput  = net_mobile->AddTensor(std::unique_ptr<Tensor>(Tensor::Create<>(
+                        "NET_INPUT_TENSOR", DataType_t::SIGNED_8BIT,
+                        TensorShape<TensorFormat_t::NHWC>{1, 224, 224,3},
+                        TensorLayout<TensorFormat_t::NHWC>())));
+
+  lApexNetInput->SetQuantParams({QuantInfo(-1, 0)});
+  lApexNetInput->Allocate(Allocation_t::OAL);
+
+
+  // Integer output of the aMnetGraph except the last 3 layers. 
+  std::vector<Tensor*> fixedOutput;
+  
+  status = LoadNetFromTensorFlow(*net_mobile, aMnetGraph, lApexNetInput, fixedOutput);
+  if(Status_t::SUCCESS != status || fixedOutput.empty()){
+    std::cout << "Failed to load net" << std::endl;
+    return -1;
+  }
+
+#ifndef _WINDOWS
+  status = net_mobile->SetTargetHint(target::APEX);
+#else
+  status = net_mobile->SetTargetHint(target::REF);
+#endif
+
+  if(Status_t::SUCCESS != status){
+    std::cout << "Set APEX target failed" << std::endl;
+    return -1;
+  }
+  
+  status = net_mobile->Prepare(); 
+
+  std::cout << "mark1" << std::endl;
+  
+  if (Status_t::SUCCESS != status){
+    std::cout << "APEX net verification failed" << std::endl;
+    return -1;
+  }
+
+std::cout << "mark3" << std::endl;
+
+  int dimN = fixedOutput[0]->Dim(3);
+  int dimH = fixedOutput[0]->Dim(2);
+  int dimW = fixedOutput[0]->Dim(1);
+  int dimC = fixedOutput[0]->Dim(0);
+  
+  Tensor* floatInterm  = netFixedToFloat->AddTensor(std::unique_ptr<Tensor>(Tensor::Create<>(
+                        "FLOAT_INTERM_TENSOR", DataType_t::FLOAT,
+                        TensorShape<TensorFormat_t::NHWC>{dimN, dimH,dimW, dimC},
+                        TensorLayout<TensorFormat_t::NHWC>())));
+  
+  floatInterm->Allocate(Allocation_t::HEAP);
+  
+  // Final Output.
+  std::vector<Tensor*> Output;
+  status = LoadNetFromTensorFlow(*netFixedToFloat, aResultGraph, floatInterm, Output);
+  if(Status_t::SUCCESS != status || Output.empty()){
+    std::cout << "Failed to load net" << std::endl;
+    return -1;
+  }
+
+  status = netFixedToFloat->SetTargetHint(target::REF);
+  if(Status_t::SUCCESS != status){
+    std::cout << "Set APEX target failed" << std::endl;
+    return -1;
+  }
+
+std::cout << "mark2" << std::endl;
+
+  status = netFixedToFloat->Prepare(); 
+  if (Status_t::SUCCESS != status){
+    std::cout << "APEX net verification failed" << std::endl;
+    return -1;
+  }
+
+  // Load class labels
+  std::ifstream labelfile(aSlimLabelsFile);
+  std::vector<std::string> classLabels;
+  std::string line;
+  
+  while(std::getline(labelfile, line)){
+      line.pop_back();
+      classLabels.push_back(line);
+  }
+
+ // std::string imagePath;
+  
+  struct Normalize norm = { 128, 128 };
+
+
+
+
+#ifndef _WINDOWS
+  io_FrameOutput_t lFrameOutput;
+  lFrameOutput.Init(DISPLAY_SCENE_WIDTH, DISPLAY_SCENE_HEIGHT, io::IO_DATA_DEPTH_08, io::IO_DATA_CH3);
+#endif
+
+
+
+  std::unique_ptr<float[]> s_result(new float[1001]);
+  float* result = s_result.get();
+
+
+
+      stopwatch(true);
+
+      resizeBilinearAndNormalize(Camera_input, lApexNetInput, true, {128}, 1.0f);
+
+     // resizeBilinearAndNormalize(outputImage, lApexNetInput, true, {128}, 1.0f);
+
+      stopwatch(false, "Resize+BN:");
+     
+      lApexNetInput->Flush();
+      fixedOutput[0]->Invalidate();
+
+
+
+
+      // Run image through parsed model
+      
+      status = net_mobile->Run();
+      if(status != Status_t::SUCCESS)
+      {
+        std::cout << "Net execution failed" << std::endl;
+         
+        return -1;
+      }
+
+      status = floatInterm->CopyDataFrom(*fixedOutput[0]);
+      if(status != Status_t::SUCCESS)
+      {
+        std::cout << "Copy failed" << std::endl;
+         
+        return -1;
+      }
+      
+      status = netFixedToFloat->Run();
+      if(status != Status_t::SUCCESS)
+      {
+        std::cout << "Fixed to float net execution failed" << std::endl;
+         
+        return -1;
+      }
+
+      int numClasses =  Output[0]-> Dim(0) *
+                        Output[0]-> Dim(1) *
+                        Output[0]-> Dim(2) * 
+                        Output[0]-> Dim(3);
+
+      softmax(Output[0], result);
+
+      
+      // Get top 5 result
+      auto results = processResults(result, numClasses);
+
+      std::cout << "Top 5: " << std::endl;
+      std::cout << std::right << std::setw(20) << classLabels[results[0].first] << ", " << std::to_string(results[0].second) << std::endl;
+      std::cout << std::right << std::setw(20) << classLabels[results[1].first] << ", " << std::to_string(results[1].second) << std::endl;
+      std::cout << std::right << std::setw(20) << classLabels[results[2].first] << ", " << std::to_string(results[2].second) << std::endl;
+      std::cout << std::right << std::setw(20) << classLabels[results[3].first] << ", " << std::to_string(results[3].second) << std::endl;
+      std::cout << std::right << std::setw(20) << classLabels[results[4].first] << ", " << std::to_string(results[4].second) << std::endl;
+      std::cout << std::endl;
+
+      
+
+#ifndef _WINDOWS
+      //DisplayImage(lFrameOutput, imagePath, classLabels, results);
+
+      stopwatch(true);
+     // DisplayImageOD(lFrameOutput, outputImage, classLabels, results); 
+      //sleep(1);
+      stopwatch(false, "DisplayImageOD():");
+
+         
+#endif
 
   return 0;
 }
